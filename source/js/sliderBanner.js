000acde1b629790a743cc645cc5a57e0e23b1d4d
Samir <sakitov@yandex.ru>
2022-11-12 10:50:31 +0300

    сделал что-бы исчезнувший слайдер был ограничен в доступе с клавиатуры и другими способами

--- source/js/sliderBanner.js
+++ source/js/sliderBanner.js
@@ -1,38 +1,41 @@
 if (document.querySelector('.slider-banner__navigation__list')) {
   const listControls = document.querySelector('.slider-banner__navigation__list');
-  const itemControl = listControls.querySelector('.slider-banner__navigation__item')
-  const listSlider = document.querySelector('.slider-banner__list')
-  const itemSlider = listSlider.querySelector('.slider-banner__item')
-  const counterSlider = document.querySelector('.slider-banner__navigation__counter')
+  const itemControl = listControls.querySelector('.slider-banner__navigation__item');
+  const listSlider = document.querySelector('.slider-banner__list');
+  const itemSlider = listSlider.querySelector('.slider-banner__item');
+  const counterSlider = document.querySelector('.slider-banner__navigation__counter');
 
   const DEFAULT_CONTROL = 1;
-  const ITEM_ACTIVE = 'slider-banner__item--active'
-  const ITEM_HIDDEN = 'slider-banner__item--hidden'
-  const CONTROL_ACTIVE = 'slider-banner__navigation__item--active'
+  const ITEM_ACTIVE = 'slider-banner__item--active';
+  const ITEM_HIDDEN = 'slider-banner__item--hidden';
+  const CONTROL_ACTIVE = 'slider-banner__navigation__item--active';
 
   let previousNumber = Number(listControls.querySelector(`.${CONTROL_ACTIVE}`).dataset.buttonNumber);
 
-  [].forEach.call(listSlider.children, function(item, i) {
-    console.log(item);
+  [].forEach.call(listSlider.children, function (item) {
+    if (!item.classList.contains(ITEM_HIDDEN) && !item.classList.contains(ITEM_ACTIVE)) {
+      item.classList.add(ITEM_HIDDEN);
+    }
   });
 
   listControls.replaceChildren();
   listControls.append(...createControls(listSlider.children.length));
 
-  listControls.addEventListener("click", onControlClick)
+  listControls.addEventListener('click', onControlClick);
 
   function onControlClick(e) {
-    const target = e.target
+    const target = e.target;
+    const data = getDataAttribute(target);
 
-    if (target.closest('.slider-banner__navigation__item')) {
-      const data = getDataAttribute(target);
-
-      setCounter(data);
-      flipSlider(data, previousNumber)
-      makeItemActive(data, previousNumber)
-
-      previousNumber = data;
+    if (!target.closest('.slider-banner__navigation__item') || listControls.children[data - 1] === listControls.children[previousNumber - 1]) {
+      return;
     }
+
+    setCounter(data);
+    flipSlider(data, previousNumber);
+    makeItemActive(data, previousNumber);
+
+    previousNumber = data;
   }
 
   function createControls(number) {
@@ -57,20 +60,47 @@ if (document.querySelector('.slider-banner__navigation__list')) {
   }
 
   function getDataAttribute(elem) {
-      return Number(elem?.dataset.buttonNumber) || Number(elem.parentElement.dataset.buttonNumber);
+    return Number(elem?.dataset.buttonNumber) || Number(elem.parentElement.dataset.buttonNumber);
   }
 
   function setCounter(number) {
     counterSlider.textContent = `0${number}`;
   }
 
-  function flipSlider(number, prevSlider) {
-    listSlider.children[prevSlider - 1].classList.remove(ITEM_ACTIVE);
-    listSlider.children[number - 1].classList.add(ITEM_ACTIVE);
+  function makeItemActive(number, prevSlider) {
+    listControls.children[prevSlider - 1].classList.remove(CONTROL_ACTIVE);
+    listControls.children[number - 1].classList.add(CONTROL_ACTIVE);
   }
 
-  function makeItemActive(number, prevItem) {
-    listControls.children[prevItem - 1].classList.remove(CONTROL_ACTIVE)
-    listControls.children[number - 1].classList.add(CONTROL_ACTIVE)
+  function flipSlider(number, prevItem) {
+    const currentSlide = listSlider.children[number - 1];
+    const previousSlide = listSlider.children[prevItem - 1];
+
+    // currentSlide.addEventListener('transitionstart', () => {
+    //   currentSlide.classList.remove(ITEM_HIDDEN);
+    //   console.log('анимация началась');
+    // });
+    //
+    // previousSlide.addEventListener('transitionend', () => {
+    //   previousSlide.classList.add(ITEM_HIDDEN);
+    //   console.log('анимация закончилась');
+    // });
+
+    previousSlide.classList.remove(ITEM_ACTIVE);
+    setTimeout(() => {
+      if (!previousSlide.classList.contains(ITEM_ACTIVE)) {
+        previousSlide.classList.add(ITEM_HIDDEN);
+      }
+    }, 700);
+
+    currentSlide.classList.remove(ITEM_HIDDEN);
+    setTimeout(() => {
+      currentSlide.classList.add(ITEM_ACTIVE);
+    }, 0);
   }
+
+  // function accessibilityItem(addAccessibility, removeAccessibility) {
+  //   removeAccessibility;
+  //   addAccessibility;
+  // }
 }
